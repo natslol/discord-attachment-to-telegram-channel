136e2d67f0b8742aeb06e277d82894b9f2a8f144
Nats <natsishere@gmail.com>
2022-04-04 01:00:23 +0200

    horribl

--- index.js
+++ index.js
@@ -5,10 +5,16 @@ const token = process.env.DISCORD_TOKEN;
 const telegram = process.env.TELEGRAM;
 const chatid = process.env.CHATID;
 
-
 bot.on('ready', () => {
-    console.log("c'est parti");
+        console.log("lol");
 })
+
+// bad english now :
+
+// i know it's horrible but idk how to return a value from an array and do like this :   
+var ext = "png" || "apng" || "avif" || "gif" || "jpeg" || "svg" || "webp" || "bpm"|| "ico" || "tiff" || "3fr" || "arw" || "cr2" || "crw" || "jpg" || "jpe" || "jfif" || "mp4" || "avi" || "mov" || "m4v";
+var video = "mp4" || "avi" || "mov" || "m4v";
+var image = "png" || "apng" || "avif" || "gif" || "jpeg" || "svg" || "webp" || "bpm"|| "ico" || "tiff" || "3fr" || "arw" || "cr2" || "crw" || "jpg" || "jpe" || "jfif";
 
 
 bot.on('message', message => {
@@ -16,20 +22,22 @@ bot.on('message', message => {
         if (message.attachments.every(attachIsImage)){
             var Attachment = (message.attachments).array();
             Attachment.forEach(function(attachment) {
-                cp.exec(`curl -X POST "https://api.telegram.org/bot${telegram}/sendPhoto" -d "chat_id=${chatid}&photo=${attachment.url}"`)
+                if(attachment.url.endsWith(video)) {
+                    cp.exec(`curl -X POST "https://api.telegram.org/bot${telegram}/sendVideo" -d "chat_id=${chatid}&video=${attachment.url}"`)
+                } else if(attachment.url.endsWith(image)) {
+                    cp.exec(`curl -X POST "https://api.telegram.org/bot${telegram}/sendPhoto" -d "chat_id=${chatid}&photo=${attachment.url}"`)
+                } else {
+                    cp.exec(`curl -X POST "https://api.telegram.org/bot${telegram}/sendMessage" -d "chat_id=${chatid}&text=${attachment.url}`)
+                }
               })
 
         }
     }
-            
-
-            
     function attachIsImage(msgAttach) {
         var url = msgAttach.url;
-        return url.indexOf("png", url.length - "png".length /*or 3*/) !== -1;
+        return url.indexOf(ext, url.length - ext.length);
     }
 
 });
 
 bot.login(token)
-
